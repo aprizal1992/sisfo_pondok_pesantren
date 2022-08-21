bc86dfe568674f6ea99f08aaaa2d347ecf7fca39
egooktafanda97 <egooktafanda1097@gmail.com>
2022-08-21 15:45:32 +0700

    -- rev ego --

--- application/views/Template/website.php
+++ application/views/Template/website.php
@@ -176,12 +176,12 @@
             <div class="copyright">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="#">uniks</a>, 2022.
+                        &copy; <a class="border-bottom" href="#">uniks --- up ---</a>, 2022.
                     </div>
                     <div class="col-md-6 text-center text-md-end">
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a class="border-bottom" href="https://htmlcodex.com">uniks</a>
-                        </br>Distributed By <a class="border-bottom" href="https://themewagon.com" target="_blank">uniks</a>
+                        Designed By <a class="border-bottom" href="https://htmlcodex.com">uniks --- up ---</a>
+                        </br>Distributed By <a class="border-bottom" href="https://themewagon.com" target="_blank">uniks --- up ---</a>
                     </div>
                 </div>
             </div>
